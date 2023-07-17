6076339867314e48fcb732e78f13cb61704a2ee8
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-07-17 18:33:42 -0300

    Added authentication through firebase

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import './App.css';
 import {Main} from "./pages/main";
 import {Login} from "./pages/Login";
+import { NavBar } from './Components/navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar/>
         <Routes>
           <Route path="/" element= {<Main />}>  </Route>
           <Route path='/login' element ={<Login />} ></Route>
--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -1,9 +1,17 @@
 import {Link} from "react-router-dom";
+import {auth} from "../../config/firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
 
 export function NavBar()
 {
+    const [user] = useAuthState(auth);
     return <div> 
         <Link to = "/"> Home</Link>
         <Link to = "/login"> Login</Link>
+
+        <div>
+            <p> {user?.displayName }</p>
+            <img src= {user?.photoURL || ""}/>
+        </div>
     </div>
 }
